1d35ea34c6b45770efd96dbcd6fbe1c0571ba2c9
Kun Lin <link@whitman.edu>
2022-03-24 11:17:11 -0700

    testing VE searchbox

--- static/js/home-searchbox.js
+++ static/js/home-searchbox.js
@@ -4,11 +4,11 @@ function searchevent() {
 		switch($('input:radio[name=search_scope_temp]:checked').val()) {
 			 case 'everything':
 			  $('#tab_select').val('default_all');
-			  $('#scopes').val('primo_central');	
+			  $('#scopes').val('primo_central');
 			  $('#primoQueryTemp').attr('placeholder','Search Whitman+Summit+Articles');
 			  break;
 			  case 'pci':
-			  $('#tab_select').val('spec_coll');
+			  $('#tab_select').val('ar_tab');
 		      $('#scopes').val('primo');
 			  $('#primoQueryTemp').attr('placeholder','Search Articles');
 			  break;
@@ -22,42 +22,42 @@ function searchevent() {
 			  $('#scopes').val('whitman_lib');
 			  $('#primoQueryTemp').attr('placeholder','Search Whitman Library');
 			  break;
-			  }				
+			  }
 	return true;
 }
 
 (function($){
-  $(function(){   
+  $(function(){
 
 	$("#primoQueryTemp").change(function(){
 		$("#primoQuery").val("any,contains," + $("#primoQueryTemp").val());
 	});
 	$('input:radio[name=search_scope_temp]').click(function() {
-		 
+
 				switch($('input:radio[name=search_scope_temp]:checked').val())
 				{
 					  case 'everything':
-					 
+
 					  $('#primoQueryTemp').attr('placeholder','Search Whitman+Summit+Articles');
 					  break;
 					  case 'pci':
-					
+
 					  $('#primoQueryTemp').attr('placeholder','Search Articles');
 					  break;
 					  case 'summit':
-							
+
 					  $('#primoQueryTemp').attr('placeholder','Search Summit and Whitman Library');
 					  break;
 					  case 'whitman':
-				
+
 					  $('#primoQueryTemp').attr('placeholder','Search Whitman Library');
 					  break;
-			}	
-				  
-		
-	
+			}
+
+
+
 	});
-	   
+
 $('#search-button').click(function() {
 		searchevent();
 	});
